3e68721acc3ff7eab55fbee57cc85341459349f8
Gediminas Venclova <gediminasvenc@gmail.com>
2025-11-20 13:28:55 +0200

    [ADD] added modules from addons-se. All history about changes could be found in branch 19.0. After mods will be availanble in OCA repo those 3 mustr be replaced: - account_statement_base - account_statement_import_camt54 - account_statement_import_camt

---
+++ account_statement_import_camt54/models/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa: F401
+
+
+from . import account_statement_import
+from . import account_journal
+from . import parser
---
+++ account_statement_import_camt54/tests/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+
+
+from . import test_get_partner_ref
+from . import test_statement
